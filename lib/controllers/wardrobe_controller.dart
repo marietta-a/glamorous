ca00e9b976a0aa31408a3631f8b1f2920bca8e67
AKUMBOM <akumbom@AKUMBOMs-MacBook-Pro.local>
2026-01-12 14:56:41 +0100

    wip: wardrobe design

---
+++ lib/controllers/wardrobe_controller.dart
@@ -0,0 +1,47 @@
+import 'package:get/get.dart';
+import 'package:glamourous/models/category.dart';
+import 'package:glamourous/models/wardrobe_item.dart';
+
+// --- GetX Controller for Wardrobe State Management ---
+class WardrobeController extends GetxController {
+
+  final RxList<Category> categories = <Category>[].obs;
+  final RxList<WardrobeItem> wardrobeItems = <WardrobeItem>[].obs;
+  
+  // Make sure this is an Rx object
+  final selectedCategory = Rxn<Category>();
+
+  @override
+  void onInit() {
+    super.onInit();
+    // Load your data
+    categories.assignAll(mockCategories);
+    wardrobeItems.assignAll(mockWardrobeItems);
+    selectedCategory.value = mockCategories.first;
+  }
+  
+  final List<Category> filteredCategories = mockCategories.obs;
+
+
+  void changeCategory(Category category) {
+    selectedCategory.value = category;
+    filterByCategory(category);
+  }
+
+  void filterByCategory(Category category){
+    try{
+      if(category == mockCategories.first){
+        filteredCategories.assignAll(categories);
+        wardrobeItems.assignAll(mockWardrobeItems);
+        return;
+      }
+      filteredCategories.assignAll(categories.where((cat) => cat.id == category.id).toList());
+      wardrobeItems.assignAll(mockWardrobeItems.where((b) =>
+                        filteredCategories.any((cat) => cat.id == b.categoryId)
+                      ).toList());
+    }
+    catch(err){
+      rethrow;
+    }
+  }
+}
