a5d0b1ff8c3e880ca22e23b411dcedd76b384deb
akumbom <akumbom5ma@gmail.com>
2026-01-13 16:15:31 +0100

    updated config

--- lib/core/config.dart
+++ lib/core/config.dart
@@ -2,5 +2,5 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 final String geminiApiKey = dotenv.env['GEMINI_API_KEY']!;
 final String glamorousApiEndpoint = dotenv.env['GLAMOUROUS_API_URL']!;
-final String supabaseUrl = dotenv.env['SUPERBASE_URL']!;
-final String supabaseAnonKey = dotenv.env['SUPERBASE_ANON_API_KEY']!;
+final String supabaseUrl = dotenv.env['SUPABASE_URL']!;
+final String supabaseAnonKey = dotenv.env['SUPABASE_ANON_API_KEY']!;
